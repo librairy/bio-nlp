a4d1bbacca89c13b5bebfbc531550c7ee85f6b05
Carlos Badenes <cbadenes@gmail.com>
2020-04-21 09:19:04 +0200

    filter by first drug

--- bioprocessor.py
+++ bioprocessor.py
@@ -88,4 +88,5 @@ class BioProcessor:
               if ("level_i" in result):
                   drug["level"] = result["level_i"]
               drugs.append(drug)
+              break
         return drugs
